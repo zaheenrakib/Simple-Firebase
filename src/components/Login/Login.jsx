a6d63f30bfa879b485d616f6c4f4a8277f0a7c55
zaheenrakib <rakibislambest@gmail.com>
2024-03-30 22:42:17 +0600

    Active Sign-In Method

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth'
+import app from '../../firebase/firebase.init';
+
+const Login = () => {
+    const [user, setUser] = useState(null);
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                setUser(loggedInUser)
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    const handleSignOut = () =>{
+        signOut(auth)
+        .then(result => {
+            console.log(result);
+            setUser(null)
+        })
+        .catch(error => {
+             console.log('Error', error);
+         })
+    }
+
+    return (
+        <div>
+            {/* user ? logout : sign in */}
+
+           {
+            user ?
+            <button onClick={handleSignOut} >Sign Out</button> :
+            <button onClick={handleGoogleSignIn}>Google Login</button>
+            
+            }
+
+
+            {user && <div>
+                <h3>User : {user.displayName} </h3>
+                <p>Email : {user.email} </p>
+                <img src={user.photoURL} alt="" />
+            </div>
+            }
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTJb6dVcUJaS_fDLuqFPCA11ptZZaoIew",
+  authDomain: "simple-firebase-278b5.firebaseapp.com",
+  projectId: "simple-firebase-278b5",
+  storageBucket: "simple-firebase-278b5.appspot.com",
+  messagingSenderId: "56884443893",
+  appId: "1:56884443893:web:f415b7e9a17f24f7b9e3bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
